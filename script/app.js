c729d00407106b9aeb27ff563556cae72e16fdc9
yinka <yinka.makanjuola@gmail.com>
2015-03-08 23:25:05 +0100

    implemented the shopping cart using services and also added purchased section that compiles a list of all purchased items and their quantity

--- script/app.js
+++ script/app.js
@@ -1,18 +1,92 @@
 (function(){
-	var app = angular.module("shop", []);
+	var app = angular.module("app", []);	
+	var purchase = [];
+	var cart = {};
+	var count =0;
 
-	app.controller('InventoryController', function(){
+	app.controller('InventoryController', function($scope, CartService){
 		this.items = items;
+		$scope.CartService = CartService;
 	});
 
+	app.controller('CartController', function($scope, CartService, PurchaseService){		
+		$scope.CartService = CartService;
+		$scope.PurchaseService = PurchaseService;
+	});
+
+	app.controller('PurchaseController', function($scope, CartService, PurchaseService){	
+		$scope.CartService = CartService;
+		$scope.PurchaseService = PurchaseService;
+		this.purchase = purchase;
+	});
+
+	app.factory('CartService', function(){
+		
+		
+
+		return{
+			get: function(){
+				return cart;
+			},
+			add: function(item){
+				if (cart[item.id]){
+					cart[item.id].incart +=1;
+				}
+					else{
+						cart[item.id] ={
+							product: item,
+							incart: 1,
+						}
+					}
+				count++;
+			},
+			remove: function(item){
+				if (cart[item.id]){
+					cart[item.id].incart -=1;
+					if(cart[item.id].incart ===0){
+						delete cart[item.id];						
+					}
+					count--;
+				}				
+			},
+			checkout: function(){
+				if (window.confirm("Do you want to pay for all these items?")) {
+				console.log(cart); 
+				  PurchaseService.add(cart);
+				}
+			},
+			count: function(){
+				return count;
+			}			
+		};
+	});
+
+	app.factory('PurchaseService', function(){
+		
+		return{
+			get: function(){
+				return purchase;
+			},
+			add: function(carts){
+				var bought = carts;				
+				angular.forEach(bought, function(value, key){
+					this.push(value);
+				}, purchase);
+				cart ={};
+				count =0;
+				console.log(purchase);
+			},		
+		};
+	});
+	
 	var items = [
-				{name: 'book', price: 2, quantity: 1,},
-				{name: 'shirt', price: 5, quantity: 1,},
-				{name: 'book', price: 2, quantity: 1,},
-				{name: 'book', price: 2, quantity: 1,},
-				{name: 'shirt', price: 5, quantity: 1,},
-				{name: 'book', price: 2, quantity: 1,},
-			
+				{id: 112, name: 'Game of Thrones', price: 200},
+				{id: 2, name: 'Eragon', price: 5000},
+				{id: 3, name: 'The Two Towers', price: 200},
+				{id: 4, name: 'The Half Blood Prince', price: 200},
+				{id: 5, name: 'Divergent', price: 5000},
+				{id: 6, name: 'The Chamber of Secrets', price: 200},					
 			];
+	
 
 })();
--- script/cart.js
+++ script/cart.js
@@ -1,3 +1,28 @@
-var Cart = function(){
-	
-}
+app.factory('CartService', function(){
+	var cart = {};
+	var count = 0;
+
+	return{
+		get: function(){
+			return cart;
+		},
+		add: function(item){
+			if (cart[item.id]){
+				cart[item.id].quantity +=1;
+			}
+				else{
+					cart[item.id] ={
+						type: item,
+						quantity: 1
+					}
+				}
+			count++;
+		},
+		remove: function(item){
+			delete cart[item.id];
+
+		}
+		count
+
+	};
+});
