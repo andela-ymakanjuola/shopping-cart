643955d17d52ee9b5bad04b6ba2badb380171e7c
yinka <yinka.makanjuola@gmail.com>
2015-03-09 17:27:57 +0100

    implementing material angular

--- script/app.js
+++ script/app.js
@@ -77,13 +77,13 @@
 		};
 	});
 	
-	var items = [
-				{id: 112, name: 'Game of Thrones',images: 'images/Game_of_thrones.jpeg', price: 200},
+	var items = [				
 				{id: 2, name: 'Eragon',images: 'images/eragon.jpg', price: 5000},
 				{id: 3, name: 'The Two Towers',images: 'images/two-towers.jpg', price: 200},
 				{id: 4, name: 'The Half Blood Prince',images: 'images/Half-Blood_Prince.jpg', price: 200},
 				{id: 5, name: 'Divergent',images: 'images/divergent.jpg', price: 5000},
-				{id: 6, name: 'The Chamber of Secrets',images: 'images/Chamber_of_Secrets.jpg', price: 200},					
+				{id: 6, name: 'The Chamber of Secrets',images: 'images/Chamber_of_Secrets.jpg', price: 200},
+				{id: 1, name: 'Game of Thrones',images: 'images/Game_of_thrones.jpeg', price: 200},					
 			];
 	
 
