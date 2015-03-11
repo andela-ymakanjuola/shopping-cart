fed1f5adbbee1e260735a7ec1a66e24ed4f6feab
yinka <yinka.makanjuola@gmail.com>
2015-03-11 10:02:53 +0100

    setup a localhost using python 2.7.9 for directives to work

--- js/app/inventory/directives/inventory.directive.js
+++ js/app/inventory/directives/inventory.directive.js
@@ -3,7 +3,7 @@ angular.module("Inventory")
 .directive("inventoryContent", function(){
 	return{
 		restrict: 'E',
-		templateUrl: 'inventory/partial/inventory-content.html'
+		templateUrl: 'js/app/inventory/partial/inventory-content.html'
 	};
 
 });
